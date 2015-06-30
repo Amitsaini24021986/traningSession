b90347e7e0fecbc67a9a58f021a86361fc2f63d0
amitsaini24021986 <amit.saini@mail.vinove.com>
2015-06-30 13:21:40 +0530

    mysecond comment

---
+++ gitxcode/gitxcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitxcode
+//
+//  Created by Amit Saini on 30/06/15.
+//  Copyright (c) 2015 Amit Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitxcode/gitxcode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitxcode
+//
+//  Created by Amit Saini on 30/06/15.
+//  Copyright (c) 2015 Amit Saini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

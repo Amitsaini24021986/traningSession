b5e71734247bef74d4a1faffc69e8afbf22ce58c
amitsaini24021986 <amit.saini@mail.vinove.com>
2015-06-30 13:54:38 +0530

    added new file

---
+++ gitxcode/newViewController.h
@@ -0,0 +1,13 @@
+//
+//  newViewController.h
+//  gitxcode
+//
+//  Created by Amit Saini on 30/06/15.
+//  Copyright (c) 2015 Amit Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newViewController : UIViewController
+
+@end
